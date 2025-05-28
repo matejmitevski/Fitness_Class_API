c2458ed14e0e68ac70bf113a30f854c065e2ae0e
Matej <matejmitevski@gmail.com>
2025-05-28 14:44:45 +0200

    finalan

--- src/main/java/com/fitness/classscheduler/model/MemberDto.java
+++
@@ -1,8 +0,0 @@
-package com.fitness.classscheduler.model;
-
-public class MemberDto {
-    public Long id;
-    public String fullName;
-    public String email;
-    public String phoneNumber;
-}
--- src/main/java/com/fitness/classscheduler/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fitness.classscheduler.repository;
-
-import com.fitness.classscheduler.model.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
